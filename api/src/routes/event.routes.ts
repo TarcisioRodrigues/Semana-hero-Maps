22280830907ccdf61e5ed73c5194108c0e49e4f4
TarcisioRodrigues <tarcisiorodrigues454@gmail.com>
2023-08-15 18:01:05 -0300

    controller e create

--- api/src/routes/event.routes.ts
+++ api/src/routes/event.routes.ts
@@ -1,13 +1,20 @@
 import { Router } from "express"
+import { EventRepositoryMogoose } from "../repositories/EventRepositoryMogoose"
+import { EventController } from "../Controller/EventController"
+import { EventUseCases } from "../useCases/EventUseCases"
 
 class EventRoutes{
     public routes:Router
+    private eventController:EventController
     constructor(){
         this.routes=Router()
+        const eventRepository=new EventRepositoryMogoose()
+        const eventUseCase=new EventUseCases(eventRepository)
+        this.eventController=new EventController(eventUseCase)
         this.initRoutes()
     }
     initRoutes(){
-        this.routes.post('/')
+        this.routes.post('/',this.eventController.create.bind(this.eventController))
     }
 }
 
