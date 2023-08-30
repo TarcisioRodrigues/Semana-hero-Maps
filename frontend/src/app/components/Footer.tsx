6a0dceaf3c2fdc9c8735797ccd83eaeecf3be2e1
TarcisioRodrigues <tarcisiorodrigues454@gmail.com>
2023-08-30 11:14:52 -0300

    front terminado

--- frontend/src/app/components/Footer.tsx
+++
@@ -1,8 +0,0 @@
-export const Footer = () => {
-  return (
-    <div className=" w-full h-[26px] fixed bottom-0 items-center bg-blue flex justify-end text-white pr-32">
-      <p>Hero Tickets*{new Date().getFullYear()}</p>
-      <p>Todos os direitos reservados</p>
-    </div>
-  );
-};
